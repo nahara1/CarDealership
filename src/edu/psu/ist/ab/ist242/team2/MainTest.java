abd3faee7143eb35f82d48476d7c40fc97e719ad
vio9j6l <jiajia.liu951ss@gmail.com>
2019-04-30 13:44:00 -0400

    Update MainTest.java

--- src/edu/psu/ist/ab/ist242/team2/MainTest.java
+++ src/edu/psu/ist/ab/ist242/team2/MainTest.java
@@ -1,3 +1,13 @@
+/*
+Project: Car Dealership
+Purpose Details: Tests the main method.
+Course: IST 242 - 001
+Team: 2
+Date Developed: 2/15/2019
+Last Date Changed: 4/29/2019
+Rev: 1
+*/
+
 package edu.psu.ist.ab.ist242.team2;
 
 import org.junit.Assert;
